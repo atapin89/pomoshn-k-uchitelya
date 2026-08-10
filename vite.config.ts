3a4cd17726c8f691f1e348860825eec6de2ec82e
atapin89 <163708847+atapin89@users.noreply.github.com>
2026-08-10 12:32:42 +0400

    Добавлен base path для GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/pomoshn-k-uchitelya/', // <-- Эта строка критически важна для GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
